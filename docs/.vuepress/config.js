af8390aa6cd834d6158e7a75feeb0d302bb19394
zhengjie <201507802@qq.com>
2021-02-08 16:00:41 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,6 +36,15 @@ module.exports = {
         ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ,async: true}],
         ['script', {},
             '(adsbygoogle = window.adsbygoogle || []).push({});'
+        ],
+
+        [
+            "script",
+            {
+                "data-ad-client": "ca-pub-3964897280370772",
+                async: true,
+                src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+            }
         ]
 
     ],
