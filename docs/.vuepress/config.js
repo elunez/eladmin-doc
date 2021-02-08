ab5f660a5354699f5af4e49cc345c6120edcd768
zhengjie <201507802@qq.com>
2021-02-08 16:10:06 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,9 +34,9 @@ module.exports = {
         ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-QTTKDL6ST0",async: true}],
 
         ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ,async: true}],
-        ['script', {},
-            '(adsbygoogle = window.adsbygoogle || []).push({});'
-        ],
+        // ['script', {},
+        //     '(adsbygoogle = window.adsbygoogle || []).push({});'
+        // ],
 
         [
             "script",
