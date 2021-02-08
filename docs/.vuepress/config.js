c83c2d6befd9b8ab7b147b9b38fa34a5eb33a8e7
zhengjie <201507802@qq.com>
2021-02-08 16:11:03 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,10 +33,10 @@ module.exports = {
         ],
         ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-QTTKDL6ST0",async: true}],
 
-        ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ,async: true}],
-        // ['script', {},
-        //     '(adsbygoogle = window.adsbygoogle || []).push({});'
-        // ],
+        // ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ,async: true}],
+        ['script', {},
+            '(adsbygoogle = window.adsbygoogle || []).push({});'
+        ],
 
         [
             "script",
