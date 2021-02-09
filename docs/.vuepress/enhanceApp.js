e1464841b6241615dd9cd5779d3822d714d9b10e
zhengjie <201507802@qq.com>
2021-02-09 13:56:37 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,35 +1,23 @@
-export default ({
-                  Vue,
-                  options,
-                  router,
-                  siteData
-                }) => {
-    /**
-     * 路由切换事件处理
-     * 路由切换
-     */
-    router.beforeEach((to, from, next) => {
-      //触发百度的pv统计
-      if (typeof _hmt != "undefined") {
-        if (to.path) {
-          _hmt.push(["_trackPageview", to.fullPath]);
-        }
-      }
-      /**
-       * 谷歌广告
-       */
-      if (typeof window !== 'undefined') {
-        import('vue-google-adsense')
-            .then(module => {
-              const Ads = module.default
-              Vue.use(require('vue-script2'))
-              Vue.use(Ads.Adsense)
-              Vue.use(Ads.InArticleAdsense)
-              Vue.use(Ads.InFeedAdsense)
-            })
-            .catch(e => {})
-      }
-      next();
-    });
-  }
+import Vue from 'vue'
+import Ads from 'vue-google-adsense'
 
+Vue.use(require('vue-script2'))
+Vue.use(Ads.Adsense)
+Vue.use(Ads.InArticleAdsense)
+Vue.use(Ads.InFeedAdsense)
+
+export default ({router}) => {
+  /**
+   * 路由切换事件处理
+   * 路由切换
+   */
+  router.beforeEach((to, from, next) => {
+    //触发百度的pv统计
+    if (typeof _hmt != "undefined") {
+      if (to.path) {
+        _hmt.push(["_trackPageview", to.fullPath]);
+      }
+    }
+    next();
+  });
+}
