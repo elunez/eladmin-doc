3217c5734aa5767ef7d6905757625cfc800f9744
Zheng Jie <201507802@qq.com>
2021-02-28 11:10:11 +0800

    update

--- docs/.vuepress/theme/components/AdSense.vue
+++ docs/.vuepress/theme/components/AdSense.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="adsense-content">
-      <Adsense data-ad-client="ca-pub-3964897280370772" data-ad-slot="6487877338" />
+    <!-- 原生 -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:256px;height:126px"
+         data-ad-client="ca-pub-3964897280370772"
+         data-ad-slot="6487877338"></ins>
   </div>
 </template>
 
 <style scoped>
 .adsense-content {
+  display: block;
   padding: 1.5rem 1.5rem 0;
   font-size: .75rem;
   max-width:256px;
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,7 +2,7 @@
   <ParentLayout>
     <InArticleAdsense style="max-width: 740px;margin: 0 auto;" data-ad-client="ca-pub-3964897280370772"
                       data-ad-slot="8192154900" slot="page-bottom" />
-<!--    <AdSense slot="sidebar-top"/>-->
+    <AdSense slot="sidebar-top"/>
   </ParentLayout>
 </template>
 
