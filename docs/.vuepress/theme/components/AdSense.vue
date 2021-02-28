74e0e11268362fb3f69e5d09cbc7514bafe4158b
Zheng Jie <201507802@qq.com>
2021-02-28 11:01:16 +0800

    update

---
+++ docs/.vuepress/theme/components/AdSense.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="adsense-content">
+      <Adsense data-ad-client="ca-pub-3964897280370772" data-ad-slot="6487877338" />
+  </div>
+</template>
+
+<style scoped>
+.adsense-content {
+  padding: 1.5rem 1.5rem 0;
+  font-size: .75rem;
+  max-width:256px;
+  max-height:126px;
+  margin: 0 auto !important;
+}
+</style>
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,14 +1,18 @@
 <template>
   <ParentLayout>
-    <InArticleAdsense style="max-width: 740px;margin: 0 auto;" data-ad-client="ca-pub-3964897280370772" data-ad-slot="8192154900" slot="page-bottom" />
+    <InArticleAdsense style="max-width: 740px;margin: 0 auto;" data-ad-client="ca-pub-3964897280370772"
+                      data-ad-slot="8192154900" slot="page-bottom" />
+    <AdSense slot="sidebar-top"/>
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import AdSense from '../components/AdSense.vue'
 export default {
   components: {
-    ParentLayout
+    ParentLayout,
+    AdSense
   }
 }
 </script>
