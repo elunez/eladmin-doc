0f62fc34259cf4ac38b750054267fa8530a119ac
Zheng Jie <201507802@qq.com>
2021-02-28 11:26:22 +0800

    update

--- docs/.vuepress/theme/components/AdSense.vue
+++ docs/.vuepress/theme/components/AdSense.vue
@@ -2,7 +2,7 @@
   <div id="el-adsense-content">
     <!-- 原生 -->
     <ins class="adsbygoogle"
-         style="display:inline-block;width:268px;height:100px"
+         style="display:inline-block;width:263px;height:100px"
          data-ad-client="ca-pub-3964897280370772"
          data-ad-slot="6487877338" />
     <script>
@@ -16,7 +16,7 @@
   display: block;
   padding: 1.0rem 1.0rem 0;
   font-size: .75rem;
-  max-width:256px;
+  max-width:263px;
   max-height:100px;
   margin: 0 auto !important;
 }
