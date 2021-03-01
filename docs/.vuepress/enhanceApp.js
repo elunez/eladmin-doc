e60422265397127202d776d1c4b87da3493a1185
Zheng Jie <201507802@qq.com>
2021-03-01 13:54:18 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import Ads from 'vue-google-adsense'
 
 Vue.use(require('vue-script2'))
-Vue.use(Ads.Adsense)
-Vue.use(Ads.InFeedAdsense)
 
 export default ({router}) => {
   /**
