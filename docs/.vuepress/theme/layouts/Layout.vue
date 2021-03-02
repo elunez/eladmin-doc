6936306d741246cc9569fa306ffc7ec1c38abd0f
Zheng Jie <201507802@qq.com>
2021-03-02 12:41:24 +0800

    update

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,14 +1,12 @@
 <template>
   <ParentLayout>
     <AdSense slot="sidebar-top"/>
-    <PostAdSense slot="page-top"/>
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
 import AdSense from '../components/AdSense.vue'
-import PostAdSense from '../components/PostAdSense.vue'
 export default {
   components: {
     ParentLayout,
