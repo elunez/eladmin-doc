eaca75b8f9f3356656d0b0a324ea613c7dd12ed2
Zheng Jie <201507802@qq.com>
2021-03-15 12:10:42 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -13,17 +13,17 @@ export default ({router}) => {
       if (to.path) {
         _hmt.push(["_trackPageview", to.fullPath]);
       }
-      // if (to.path !== from.path && to.path !== '/' && from.name !== null) {
-      //   let paras = document.getElementById('el-adsense-content');
-      //   if (paras){
-      //     paras.innerHTML = '';
-      //     paras.innerHTML = '<ins class="adsbygoogle"\n' +
-      //         '     style="display:inline-block;width:266px;height:120px"\n' +
-      //         '     data-ad-client="ca-pub-3964897280370772"\n' +
-      //         '     data-ad-slot="4998200680"></ins>';
-      //     (window.adsbygoogle || []).push({});
-      //   }
-      // }
+      if (to.path !== from.path && to.path !== '/' && from.name !== null) {
+        let paras = document.getElementById('el-adsense-content');
+        if (paras){
+          paras.innerHTML = '';
+          paras.innerHTML = '<ins class="adsbygoogle"\n' +
+              '     style="display:inline-block;width:266px;height:120px"\n' +
+              '     data-ad-client="ca-pub-3964897280370772"\n' +
+              '     data-ad-slot="4998200680" />';
+          (window.adsbygoogle || []).push({});
+        }
+      }
     }
     next();
   });
--- docs/.vuepress/theme/components/AdSense.vue
+++ docs/.vuepress/theme/components/AdSense.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="el-adsense-content">
     <!-- 原生 -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- 侧栏广告 -->
     <ins class="adsbygoogle"
          style="display:inline-block;width:266px;height:120px"
          data-ad-client="ca-pub-3964897280370772"
-         data-ad-slot="4998200680"/>
+         data-ad-slot="4998200680"></ins>
     <script>
       (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
