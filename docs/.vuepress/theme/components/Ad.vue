cd2cda71e281a06d4ee382bae105fba74e47d8e3
Zheng Jie <201507802@qq.com>
2021-12-03 23:12:49 +0800

    update

--- docs/.vuepress/theme/components/Ad.vue
+++ docs/.vuepress/theme/components/Ad.vue
@@ -2,7 +2,7 @@
   <div id="el-adsense-content">
     <!-- 侧栏 -->
     <ins class="adsbygoogle"
-         style="display:inline-block;width:260px;height:130px"
+         style="display:inline-block;width:260px;height:120px"
          data-ad-client="ca-pub-3964897280370772"
          data-ad-slot="5748135954"/>
   </div>
@@ -14,7 +14,7 @@
   padding: 0.7rem 0.7rem 0;
   font-size: .75rem;
   max-width:266px;
-  max-height:130px;
+  max-height:120px;
   margin: 0 auto !important;
 }
 </style>
