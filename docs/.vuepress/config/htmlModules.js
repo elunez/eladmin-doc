fc587ab7b1266b7025698a6272d3f2365d1d1ec0
Zheng Jie <201507802@qq.com>
2022-02-09 15:41:38 +0800

    update

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -21,10 +21,10 @@
 // 万维
 module.exports = {
   pageB: `
-    <div class="wwads-cn wwads-horizontal" data-id="135" style="width:100%;max-height:90px;"></div>
+    <div class="wwads-cn wwads-horizontal" data-id="148" style="width:100%;max-height:90px;"></div>
   `,
   windowRB: `
-    <div class="wwads-cn wwads-vertical" data-id="135" style="max-width:200px;"></div>
+    <div class="wwads-cn wwads-vertical" data-id="148" style="max-width:200px;"></div>
     <script>
         var ele = document.querySelector('.wwads-vertical').parentElement.parentElement;
         ele.style.maxHeight = 'unset';
