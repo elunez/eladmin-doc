16242ba6975c04f142c39d5fda4c195c913df149
Zheng Jie <201507802@qq.com>
2022-02-23 21:33:33 +0800

    AD

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -10,6 +10,7 @@ module.exports = [
     }
   ],
   ["meta", { name: "theme-color", content: "#11a8cd" }],
+  ["script", { async: true, src: "https://cdn.wwads.cn/js/makemoney.js" }],
   ['script', {}, `
     // 万维广告“禁止”广告拦截
     function ABDetected() {
