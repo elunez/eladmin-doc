8a913799cbffa6e9858a965a2a8452e49176f082
Zheng Jie <201507802@qq.com>
2022-08-02 14:08:04 +0800

    update

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -10,6 +10,7 @@ module.exports = [
   ],
   ["meta", { name: "theme-color", content: "#11a8cd" }],
   ["meta", { "http-equiv": "Content-Type", content: "text/html;charset=gb2312" }],
+  ["meta", { name: "baidu-site-verification", content: "code-kdORRzs7wW" }],
   ["meta", { name: "sogou_site_verification", content: "K0Czk3nRO0" }],
   ["meta", { name: "360-site-verification", content: "0f840cb9fd8111aa6014e43d60b6ed79" }],
   ["script", { async: true, src: "https://cdn.wwads.cn/js/makemoney.js" }],
