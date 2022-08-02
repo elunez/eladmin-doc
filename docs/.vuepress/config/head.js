012d400786dc212d5dfab43c1bd22fad2488109c
Zheng Jie <201507802@qq.com>
2022-08-02 14:09:28 +0800

    update

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -11,8 +11,8 @@ module.exports = [
   ["meta", { name: "theme-color", content: "#11a8cd" }],
   ["meta", { "http-equiv": "Content-Type", content: "text/html;charset=gb2312" }],
   ["meta", { name: "baidu-site-verification", content: "code-kdORRzs7wW" }],
-  ["meta", { name: "sogou_site_verification", content: "K0Czk3nRO0" }],
-  ["meta", { name: "360-site-verification", content: "0f840cb9fd8111aa6014e43d60b6ed79" }],
+  ["meta", { name: "sogou_site_verification", content: "M7FZGnsiqa" }],
+  ["meta", { name: "360-site-verification", content: "f6ae0ca805b4ad57430f50784d798c2e" }],
   ["script", { async: true, src: "https://cdn.wwads.cn/js/makemoney.js" }],
   ['script', {}, `
     // 万维广告“禁止”广告拦截
