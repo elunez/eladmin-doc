aa57d985d59dee1b81f25f1ba647da74254992e4
Zheng Jie <201507802@qq.com>
2022-08-03 17:43:44 +0800

    update

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,13 +1,7 @@
 // Head Config
 module.exports = [
   ["link", { rel: "icon", href: "/logo/small.png" }],
-  [
-    "meta",
-    {
-      name: "keywords",
-      content: "eladmin,el-admin,eladmin官网,eladmin在线文档,eladmin学习"
-    }
-  ],
+  ["meta", { name: "keywords", content: "eladmin,el-admin,eladmin官网,eladmin在线文档,eladmin学习" }],
   ["meta", { name: "theme-color", content: "#11a8cd" }],
   ["meta", { "http-equiv": "Content-Type", content: "text/html;charset=gb2312" }],
   ["meta", { name: "baidu-site-verification", content: "code-kdORRzs7wW" }],
@@ -15,50 +9,7 @@ module.exports = [
   ["meta", { name: "360-site-verification", content: "f6ae0ca805b4ad57430f50784d798c2e" }],
   ["script", { async: true, src: "https://cdn.wwads.cn/js/makemoney.js" }],
   ['script', {}, `
-    // 万维广告“禁止”广告拦截
-    function ABDetected() {
-      var adBlockDetected_div = document.createElement("div");
-      adBlockDetected_div.id = "el-remind"
-      adBlockDetected_div.style.cssText = "position: absolute; top: 0; left: 0; width: 100%; background: #fc6600; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
-      adBlockDetected_div.innerHTML = "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本项目的长期发展，请将本网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
-      document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);
-      // add a close button to the right side of the div
-      var adBlockDetected_close = document.createElement("div");
-      adBlockDetected_close.style.cssText = "position: absolute; top: 0; right: 10px; width: 30px; height: 30px; background: #fc6600; color: #fff; z-index: 9999999999; line-height: 30px; cursor: pointer;";
-      adBlockDetected_close.innerHTML = "×";
-      adBlockDetected_div.appendChild(adBlockDetected_close);
-      // add a click event to the close button
-      adBlockDetected_close.onclick = function() {
-      this.parentNode.parentNode.removeChild(this.parentNode);
-      };
-    }
-
-    function docReady(t) {
-      "complete" === document.readyState ||
-      "interactive" === document.readyState
-        ? setTimeout(t, 1)
-        : document.addEventListener("DOMContentLoaded", t);
-    }
-
-    //check if wwads' fire function was blocked after document is ready with 3s timeout (waiting the ad loading)
-    docReady(function () {
-      setTimeout(function () {
-        if( window._AdBlockInit === undefined ){
-            ABDetected();
-        }
-      }, 3000);
-    });
-  `],
-  [
-    "script",
-    {}, `
-            var _hmt = _hmt || [];
-            (function() {
-            let aHtml = document.createElement('a');
-              var hm = document.createElement("script");
-              hm.src = "//hm.baidu.com/hm.js?300a24a5326d0e7a0ff1b14c4e8d9056";
-              var s = document.getElementsByTagName("script")[0]; 
-              s.parentNode.insertBefore(hm, s);
-            })();`
-  ]
+    function ABDetected(){var adBlockDetected_div=document.createElement("div");adBlockDetected_div.id="el-remind";adBlockDetected_div.style.cssText="position: absolute; top: 0; left: 0; width: 100%; background: #fc6600; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";adBlockDetected_div.innerHTML="我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本项目的长期发展，请将本网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);var adBlockDetected_close=document.createElement("div");adBlockDetected_close.style.cssText="position: absolute; top: 0; right: 10px; width: 30px; height: 30px; background: #fc6600; color: #fff; z-index: 9999999999; line-height: 30px; cursor: pointer;";adBlockDetected_close.innerHTML="×";adBlockDetected_div.appendChild(adBlockDetected_close);adBlockDetected_close.onclick=function(){this.parentNode.parentNode.removeChild(this.parentNode)}}function docReady(t){"complete"===document.readyState||"interactive"===document.readyState?setTimeout(t,1):document.addEventListener("DOMContentLoaded",t)}docReady(function(){setTimeout(function(){if(window._AdBlockInit===undefined){ABDetected()}},3e3)});
+    var _hmt=_hmt||[];(function(){let aHtml=document.createElement("a");var hm=document.createElement("script");hm.src="//hm.baidu.com/hm.js?300a24a5326d0e7a0ff1b14c4e8d9056";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();
+  `]
 ];
