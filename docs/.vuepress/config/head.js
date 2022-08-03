4b91cc29c0f5afee63e725d5a71988d9f87d929d
Zheng Jie <201507802@qq.com>
2022-08-03 17:32:15 +0800

    update

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -18,8 +18,9 @@ module.exports = [
     // 万维广告“禁止”广告拦截
     function ABDetected() {
       var adBlockDetected_div = document.createElement("div");
+      adBlockDetected_div.id = "el-remind"
       adBlockDetected_div.style.cssText = "position: absolute; top: 0; left: 0; width: 100%; background: #fc6600; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
-      adBlockDetected_div.innerHTML = "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本站的长期运行，请将我们的网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
+      adBlockDetected_div.innerHTML = "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本项目的长期发展，请将本网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
       document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);
       // add a close button to the right side of the div
       var adBlockDetected_close = document.createElement("div");
