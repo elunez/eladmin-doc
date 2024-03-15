5cd024acab302c2d087b8d8485f98c2917c39e67
Zheng Jie <201507802@qq.com>
2024-03-15 09:22:47 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -12,6 +12,5 @@ module.exports = [
   { text: "常见问题", link: "/pages/020101/" },
   { text: "更新日志", link: "/pages/060101/" },
   { text: "支持项目", link: "/pages/030101/" },
-  { text: "VPS推荐", link: "/pages/040101/" },
   { text: "在线体验", link: "https://eladmin.vip/demo/" },
 ];
