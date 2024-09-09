0ffe5e9b7b7bf19c56ce68b86faa7bc259343fbf
Zheng Jie <201507802@qq.com>
2024-09-09 13:24:25 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,22 +4,18 @@ export default ({
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
+    import('vue-google-adsense').then(module => {
+        const Ads = module.default
+        Vue.use(require('vue-script2'))
+        Vue.use(Ads.Adsense)
+        Vue.use(Ads.InArticleAdsense)
+        Vue.use(Ads.InFeedAdsense)
+    }).catch(e => {console.log(e)})
   /**
    * 路由切换事件处理
    */
   router.beforeEach((to, from, next) => {
     //触发百度的pv统计
-    import('vue-google-adsense')
-          .then(module => {
-              const Ads = module.default
-              Vue.use(require('vue-script2'))
-              Vue.use(Ads.Adsense)
-              Vue.use(Ads.InArticleAdsense)
-              Vue.use(Ads.InFeedAdsense)
-          })
-          .catch(e => {
-              console.log(e)
-    })
     if (typeof _hmt != "undefined") {
       if (to.path) {
         _hmt.push(["_trackPageview", to.fullPath]);
